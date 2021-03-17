60d60612fa1ffb223e5519f8e6e7b629a04b5c70
Kaylany Paulus <kaylanysilvapaulus@gmail.com>
2021-03-17 19:47:36 -0300

    Cidades do litoral sul.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿    using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        //Lista com o nome das cidade do litoral sul
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Cidades do litoral Sul: \n");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Bertioga");
+            Console.WriteLine("Guarujá");
+            Console.WriteLine("Santos");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Peruibe");
+            Console.WriteLine("Iguape");
+            Console.WriteLine("Ilha Comprida");
+            Console.WriteLine("Cananéia");
+            Console.ResetColor();
+            Console.ReadKey();
+            Console.Clear();
+       }
+    }
+}
